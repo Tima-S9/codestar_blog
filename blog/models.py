cf1ce5c7e6d836272771c5f9b6af053925376177
Tima-S9 <fatimaadora@yahoo.fr>
2025-07-14 12:51:12 +0100

    adds a Post model

--- blog/models.py
+++ blog/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
+STATUS = ((0, "Draft"), (1, "Published"))
 
 
 class Post(models.Model):
-    STATUS = ((0, "Draft"), (1, "Published"))
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(
