66964c98429cb018c9b3b6917c3e77f7b36b419c
Tima-S9 <fatimaadora@yahoo.fr>
2025-07-16 15:39:09 +0100

    added post_detail view

--- codestar/urls.py
+++ codestar/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path("", include("blog.urls"), name="blog-urls"),
     path('summernote/', include('django_summernote.urls')),
     path('admin/', admin.site.urls),  
+    path('<slug:slug>/', views.post_detail, name='post_detail'),
 ]
